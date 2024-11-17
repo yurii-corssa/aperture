32f8ae62f8ad50d8f01cf712a1bd499d02db8f21
yurii-corssa <122684054+yurii-corssa@users.noreply.github.com>
2024-11-17 12:57:57 +0000

    Deploying to gh-pages from @ yurii-corssa/aperture@d7827bb24ff39e554b568b649182957e7b55845d 🚀

--- assets/index-CeuubZSH.js
+++ assets/index-CeuubZSH.js
@@ -133,7 +133,94 @@ const fluidText = () => {
       processNode(textElement);
     });
     textBlock.classList.add("fluid-text");
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(
+        (entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("animate");
+          } else {
+            entry.target.classList.remove("animate");
+          }
+        },
+        {
+          threshold: 0.9
+        }
+      );
+    });
+    observer.observe(textBlock);
   });
+};
+const observeElements = () => {
+  const elements = document.querySelectorAll("[data-watch]");
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        } else {
+          entry.target.classList.remove("visible");
+        }
+      });
+    },
+    {
+      // rootMargin: "-20% 0px -20% 0px",
+    }
+  );
+  elements.forEach((element) => {
+    observer.observe(element);
+  });
+};
+const parallax = () => {
+  const elements = [...document.querySelectorAll("[data-prlx-parent]")];
+  if (elements.length) {
+    elements.forEach((parent) => {
+      const elements2 = parent.querySelectorAll("[data-prlx]");
+      let animationID;
+      let value = 0;
+      const animationFrame = () => {
+        const topToWindow = parent.getBoundingClientRect().top;
+        const heightParent = parent.offsetHeight;
+        const heightWindow = window.innerHeight;
+        const positionParent = {
+          top: topToWindow - heightWindow,
+          bottom: topToWindow + heightParent
+        };
+        let offset = 0;
+        const centerPoint = parent.dataset.prlxCenter || "center";
+        const smooth = parent.dataset.prlxSmooth ? Number(parent.dataset.prlxSmooth) : 15;
+        if (positionParent.top < 30 && positionParent.bottom > -30) {
+          switch (centerPoint) {
+            case "top":
+              offset = -1 * topToWindow;
+              break;
+            case "center":
+              offset = heightWindow / 2 - (topToWindow + heightParent / 2);
+              break;
+            case "bottom":
+              offset = heightWindow - (topToWindow + heightParent);
+              break;
+          }
+        }
+        value += (offset - value) / smooth;
+        animationID = window.requestAnimationFrame(animationFrame);
+        elements2.forEach((element, index) => {
+          const coefficient = element.dataset.coefficient ? Number(element.dataset.coefficient) : 5;
+          element.style.transition = `all 0.1s ${(elements2.length + index) * 10 / coefficient}ms`;
+          element.style.transform = `translate3D(0, ${-1 * (value / coefficient).toFixed(2)}px,0)`;
+        });
+      };
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            animationID = window.requestAnimationFrame(animationFrame);
+          } else {
+            window.cancelAnimationFrame(animationID);
+          }
+        });
+      });
+      observer.observe(parent);
+    });
+  }
 };
 const trackScrollPosition = (targetElement = document.documentElement, variableName = "--scroll-position") => {
   targetElement.style.setProperty(variableName, window.scrollY);
@@ -142,5 +229,8 @@ const trackScrollPosition = (targetElement = document.documentElement, variableN
     targetElement.style.setProperty(variableName, scrollPosition);
   });
 };
-const utils = { fluidText, trackScrollPosition };
+const utils = { fluidText, trackScrollPosition, observeElements, parallax };
+utils.fluidText();
 utils.trackScrollPosition();
+utils.observeElements();
+utils.parallax();
