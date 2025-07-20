35600ccb716e17918a496162eb5c67b7a64711c9
yurii-corssa <yurii.corssa@gmail.com>
2025-07-20 15:02:35 +0200

    add Newsletter class for newsletter form handling and success modal integration

---
+++ src/js/Newsletter.js
@@ -0,0 +1,47 @@
+import { SuccessModal } from "./SuccessModal";
+
+export class Newsletter {
+  constructor() {
+    this.selectors = {
+      FORM: "[data-newsletter-form]",
+    };
+    this.elements = {};
+    this.elements.form = document.querySelector(this.selectors.FORM);
+
+    this.successModal = new SuccessModal();
+  }
+
+  submitForm = (formEntries) => {
+    const successMessage = {
+      title: `Thank you for subscribing!`,
+      text: `You have successfully subscribed with the email: ${formEntries.email}.`,
+    };
+
+    this.successModal.openModal(successMessage);
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(this.elements.form);
+    const formEntries = Object.fromEntries(formData.entries());
+    this.submitForm(formEntries);
+    this.elements.form.reset();
+  };
+
+  handleIntersect = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log("Form is visible");
+        this.elements.form.addEventListener("submit", this.handleSubmit);
+      } else {
+        console.log("Form is not visible");
+        this.elements.form.removeEventListener("submit", this.handleSubmit);
+      }
+    });
+  };
+
+  init() {
+    this.intersectObserver = new IntersectionObserver(this.handleIntersect);
+    this.intersectObserver.observe(this.elements.form);
+  }
+}
