c6028920649b7e134577d74d4f5ec662600fb4ea
yurii-corssa <yurii.corssa@gmail.com>
2025-07-27 18:37:41 +0200

    Implement GalleryModal class

---
+++ src/js/GalleryModal.js
@@ -0,0 +1,49 @@
+import { Modal } from "./Modal";
+import { Slider } from "./Slider";
+
+export class GalleryModal extends Modal {
+  constructor({ key = "gallery" }) {
+    super({
+      MODAL: `[data-modal-name='${key}']`,
+      OPEN_BUTTON: `[data-modal-open='${key}']`,
+      CLOSE_BUTTON: "[data-modal-close]",
+    });
+
+    this.sliderConfig = {
+      key,
+      slidesToShow: 1,
+    };
+
+    this.elements.openButton = document.querySelector(this.selectors.OPEN_BUTTON);
+
+    this.gallerySlider = new Slider(this.sliderConfig);
+  }
+
+  openModal() {
+    super.openModal();
+    this.elements.openButton.removeEventListener("click", this.openModal);
+
+    this.gallerySlider.init();
+  }
+
+  closeModal() {
+    super.closeModal();
+    this.elements.openButton.addEventListener("click", this.openModal);
+    this.gallerySlider.destroy();
+  }
+
+  handleIntersect = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        this.elements.openButton.addEventListener("click", this.openModal);
+      } else {
+        this.elements.openButton.removeEventListener("click", this.openModal);
+      }
+    });
+  };
+
+  init() {
+    this.intersectObserver = new IntersectionObserver(this.handleIntersect);
+    this.intersectObserver.observe(this.elements.openButton);
+  }
+}
