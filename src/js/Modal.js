c146a3caf9b356d40486aac29d15aad8118154f1
yurii-corssa <yurii.corssa@gmail.com>
2025-08-16 22:38:50 +0200

    refactor modals to use <dialog> element

--- src/js/Modal.js
+++ src/js/Modal.js
@@ -1,5 +1,3 @@
-import { removeAllInert, removeInert, setInert, setInertOutside } from "./utils";
-
 export class Modal {
   constructor(selectors, classes) {
     this.selectors = {
@@ -10,8 +8,6 @@ export class Modal {
       ...selectors,
     };
     this.classes = {
-      IS_VISIBLE: "is-visible",
-      IS_HIDDEN: "is-hidden",
       NO_SCROLL: "no-scroll",
       ...classes,
     };
@@ -20,9 +16,6 @@ export class Modal {
     this.elements.modalsContainer = document.querySelector(this.selectors.MODALS_CONTAINER);
     this.elements.modal = this.elements.modalsContainer.querySelector(this.selectors.MODAL);
     this.elements.modalWindow = this.elements.modal.querySelector(this.selectors.MODAL_WINDOW);
-    this.elements.trigger = null;
-    this.elements.modal.classList.add(this.classes.IS_HIDDEN);
-
     this.openModal = this.openModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
   }
@@ -43,15 +36,9 @@ export class Modal {
   };
 
   openModal() {
-    this.elements.trigger = document.activeElement;
-
-    removeInert(this.elements.modalsContainer);
-    setInertOutside(this.elements.modalsContainer);
-
     document.body.classList.add(this.classes.NO_SCROLL);
-    this.elements.modal.classList.replace(this.classes.IS_HIDDEN, this.classes.IS_VISIBLE);
-    this.elements.modal.setAttribute("aria-hidden", "false");
-    this.elements.modal.setAttribute("aria-modal", "true");
+
+    this.elements.modal.showModal();
 
     setTimeout(() => {
       document.addEventListener("click", this.handleClick);
@@ -60,16 +47,9 @@ export class Modal {
   }
 
   closeModal() {
-    removeAllInert();
-    setInert(this.elements.modalsContainer);
-
-    if (this.elements.trigger) this.elements.trigger.focus();
-    else document.body.focus();
-
     document.body.classList.remove(this.classes.NO_SCROLL);
-    this.elements.modal.classList.replace(this.classes.IS_VISIBLE, this.classes.IS_HIDDEN);
-    this.elements.modal.setAttribute("aria-hidden", "true");
-    this.elements.modal.setAttribute("aria-modal", "false");
+
+    this.elements.modal.close();
 
     document.removeEventListener("click", this.handleClick);
     window.removeEventListener("keydown", this.handleKeyDown);
