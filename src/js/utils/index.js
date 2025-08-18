8557195d278ea96bcb7ba05c2385510f615409bd
yurii-corssa <yurii.corssa@gmail.com>
2025-08-18 22:47:24 +0200

    remove inert utility

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -1,4 +1,3 @@
 export { createObserver } from "./observer";
 export { traverseTextNodes } from "./traverse-text-nodes";
 export { toRem } from "./to-rem";
-export { setInert, removeInert, setInertOutside, removeAllInert } from "./inert";
